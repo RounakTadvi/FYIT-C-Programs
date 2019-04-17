4128425bdeffc0c2e704098f6a831fa9a5612b21
Rounak Tadvi <38634459+RounakTadvi@users.noreply.github.com>
2019-04-17 14:04:15 +0530

    Add files via upload

---
+++ Practical 1/p1.4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+class circle{
+    float area , radius;
+    public:
+    void read();
+    void compute();
+    void display();
+};
+
+void circle :: read(){
+    cout << "Enter Radius of Circle: " << endl;
+     cin >> radius;
+}
+void circle :: compute(){
+    area = 3.14 * radius * radius;
+}
+void circle :: display(){
+    cout << "Area of Circle: " << area;
+}
+
+int main(){
+    circle c;
+    c.read();
+    c.compute();
+    c.display();
+    getch();
+    return 0;
+}
