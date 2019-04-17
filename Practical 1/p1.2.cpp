fcaf99397e15bbf91be321fc718e0c778d5f9818
Rounak Tadvi <38634459+RounakTadvi@users.noreply.github.com>
2019-04-17 13:50:44 +0530

    Add files via upload

---
+++ Practical 1/p1.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class student{
+    private:
+    char name[30];
+    int roll_no;
+    void getData(){
+        cout << "Enter Student name:" << endl;
+        cin >> name;
+        cout << "Enter Roll Number:" << endl;
+        cin >> roll_no;
+    }
+    public:
+    void displayData(){
+        getData();
+        cout << "Student Name:" << name << endl;
+        cout << "Roll Number:" << roll_no << endl;
+    }
+};
+
+int main(){
+    student s;
+    s.displayData();
+    getch();
+    return 0;
+}
